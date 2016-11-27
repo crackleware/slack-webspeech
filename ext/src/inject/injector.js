d8c5eaa182159c242ba0a003130bfd37964f60a3
pedja <pedja@crackleware.nu>
2016-11-27 22:32:15 +0100

    extension page action button toggles speech recognition and TTS

--- ext/src/inject/injector.js
+++ ext/src/inject/injector.js
@@ -6,11 +6,12 @@ chrome.extension.sendMessage({}, function(response) {
             chrome.storage.sync.get({
                 lang: 'en-US',
                 enableTTS: false,
-                voiceName: 'Google US English'
+                voiceName: 'Google US English',
+                prependUsername: false
             }, function(items) {
                 console.log('storage items:', items);
                 var s = document.createElement('script');
-                s.textContent = '('+startSlackWebspeechInPage+')('+JSON.stringify(items)+')';
+                s.textContent = '('+toggleSlackWebspeechInPage+')('+JSON.stringify(items)+')';
                 (document.head || document.documentElement).appendChild(s);
             });
 	    }
@@ -18,7 +19,7 @@ chrome.extension.sendMessage({}, function(response) {
 });
 
 
-function startSlackWebspeechInPage(opts) {
+function toggleSlackWebspeechInPage(opts) {
     function log() {
         var a = Array.prototype.slice.call(arguments, 0);
         console.log.apply(console, ['SlackWebspeech'].concat(a)); 
@@ -31,7 +32,19 @@ function startSlackWebspeechInPage(opts) {
             clearInterval(sws.flushDelayInterval);
         }
         $('#msgs_div')[0].removeEventListener("DOMNodeInserted", sws.onNodeInserted, false);
+
+        if (sws.enableTTS) {
+            sws.voiceSynth.cancel();
+        }
+
+        sws.start_recognition = function () { };
         sws.stop_recognition();
+
+        $('#slack-webspeech-status').remove();
+        
+        delete window.slackWebspeech;
+        
+        return;
     }
 
     log('starting new', opts);
@@ -40,8 +53,14 @@ function startSlackWebspeechInPage(opts) {
     sws.lang = opts.lang;
     sws.enableTTS = opts.enableTTS;
     sws.voiceName = opts.voiceName;
+    sws.prependUsername = opts.prependUsername;
     
     sws.flushDelay = 2000; // ms
+
+
+    $('.messages_header .channel_title').after(
+        '<div id="slack-webspeech-status" style="min-width: 250px"><pre></pre></div>')
+    $('#slack-webspeech-status pre').text('Slack Webspeech enabled');
 
     sws.recognition = new webkitSpeechRecognition();
     sws.recognition.continuous = true;
@@ -56,6 +75,13 @@ function startSlackWebspeechInPage(opts) {
         log('send_message:', sws.final_transcript);
         $('#message-input')[0].value = sws.final_transcript; TS.view.submit(); // slack integration
         sws.final_transcript = '';
+        sws.update_status('', sws.final_transcript);
+    }
+
+    sws.update_status = function(interim_transcript, final_transcript) {
+        $('#slack-webspeech-status pre').html(
+            'interim: ' + interim_transcript.trim() + '\n' +
+            '  final: <b>' + final_transcript.trim() + '</b>');
     }
 
     sws.recognition.onstart = function() {
@@ -64,12 +90,13 @@ function startSlackWebspeechInPage(opts) {
     sws.recognition.onresult = function(event) {
         //log('webspeech onresult', event);
 
+        sws.lasttime = sws.time();
+
         var interim_transcript = '';
 
         for (var i = event.resultIndex; i < event.results.length; ++i) {
             if (event.results[i].isFinal) {
                 sws.final_transcript += event.results[i][0].transcript;
-                sws.lasttime = sws.time();
             } else {
                 interim_transcript += event.results[i][0].transcript;
             }
@@ -77,6 +104,8 @@ function startSlackWebspeechInPage(opts) {
         log('final_transcript:', sws.final_transcript);
         log('interim_transcript:', interim_transcript);
 
+        sws.update_status(interim_transcript, sws.final_transcript);
+        
         if (0) {
             var send = false;
             ['.', '!', '?'].forEach(function (c) {
@@ -93,7 +122,7 @@ function startSlackWebspeechInPage(opts) {
     sws.recognition.onend = function() {
         log('webspeech onend');
         if (sws.recognition_keep_running) {
-            setTimeout(function () { sws.recognition.start(); }, 50);
+            setTimeout(function () { sws.recognition.start(); }, 10);
         }
     }
 
@@ -110,6 +139,8 @@ function startSlackWebspeechInPage(opts) {
         }, 200); 
     }
 
+    //setInterval(function () { console.log(sws.recognition_keep_running); }, 500);
+
     sws.start_recognition = function() {
         sws.recognition_keep_running = true;
         sws.recognition.start();
@@ -125,14 +156,22 @@ function startSlackWebspeechInPage(opts) {
 
         sws.voice = null;
         // voices needs some time to be ready
-        sws.voiceSynth.getVoices().forEach(function (v) {
-            log('found voice:', [v.name, v.lang]);
-            if (v.name == sws.voiceName) {
-                log('selecting voice:', [v.name, v.lang]);
-                sws.voice = v;
+        sws.voiceSynth.onvoiceschanged = function() {
+            if (sws.voice) return;
+            var voices = '';
+            sws.voiceSynth.getVoices().forEach(function (v) {
+                voices += v.name+' ('+v.lang+'), ';
+                if (v.name == sws.voiceName) {
+                    sws.voice = v;
+                }
+            });
+            log('found voices:', voices);
+            if (sws.voice) {
+                log('selected voice:', [sws.voice.name, sws.voice.lang]);
+                $('#slack-webspeech-status pre').text('Found TTS voice.');
             }
-        });
-
+        };
+        
         sws.speak_message = function(msg) {
             log('speak_message:', msg);
             var ut = new SpeechSynthesisUtterance(msg);
@@ -158,7 +197,9 @@ function startSlackWebspeechInPage(opts) {
                 //if (el.attr('data-member-id') != my_slack_id) {
                 if (username != sws.my_slack_username) {
                     var el_body = el.find('.message_body').first();
-                    var text = username + 'says, ' + el_body.text();
+                    var text = '';
+                    if (sws.prependUsername) text += username + 'says, ';
+                    text += el_body.text();
                     sws.speak_message(text);
                 }
             }
--- ext/src/options/options.js
+++ ext/src/options/options.js
@@ -2,10 +2,12 @@ function save_options() {
     var lang = document.getElementById('lang').value;
     var enableTTS = document.getElementById('enableTTS').checked;
     var voiceName = document.getElementById('voiceName').value;
+    var prependUsername = document.getElementById('prependUsername').checked;
     chrome.storage.sync.set({
         lang: lang,
         enableTTS: enableTTS,
-        voiceName: voiceName
+        voiceName: voiceName,
+        prependUsername: prependUsername
     }, function() {
         var status = document.getElementById('status');
         status.textContent = 'Options saved.';
@@ -19,11 +21,13 @@ function restore_options() {
     chrome.storage.sync.get({
         lang: 'en-US',
         enableTTS: false,
-        voiceName: 'Google US English'
+        voiceName: 'Google US English',
+        prependUsername: true
     }, function(items) {
         document.getElementById('lang').value = items.lang;
         document.getElementById('enableTTS').checked = items.enableTTS;
         document.getElementById('voiceName').value = items.voiceName;
+        document.getElementById('prependUsername').value = items.prependUsername;
     });
 }
 
